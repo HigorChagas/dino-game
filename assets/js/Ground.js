eecaf5138907be2ca48aeacf80f6341ea1f54bdf
HigorChagas <higornchagas@gmail.com>
2023-01-13 16:10:52 -0300

    Criando chão do jogo

---
+++ assets/js/Ground.js
@@ -0,0 +1,16 @@
+export default class Ground {
+    constructor(ctx, width, height, speed, scaleRatio) {
+        this.ctx = ctx;
+        this.canvas = ctx.canvas;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.scaleRatio = scaleRatio;
+
+        this.y = 0;
+        this.y = this.canvas.height = this.height;
+
+        this.groundImage = new Image();
+        this.groundImage.src = 'images/ground.png';
+    }
+}
