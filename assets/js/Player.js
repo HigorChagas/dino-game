aa0a56f783af5b75af7fe9d3f5277ca9fce975dc
Higor Chagas <higornchagas@gmail.com>
2023-01-14 23:15:23 -0300

    Adicionando imagens de animação do dinossauro

--- assets/js/Player.js
+++ assets/js/Player.js
@@ -1,4 +1,8 @@
 export default class Player {
+    WALK_ANIMATION_TIMER = 200;
+    walkAnimationTimer = this.WALK_ANIMATION_TIMER;
+    dinoRunImages = [];
+
     constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio) {
         this.ctx = ctx;
         this.canvas = ctx.canvas;
@@ -14,10 +18,16 @@ export default class Player {
         this.standingStillImage = new Image();
         this.standingStillImage.src = 'images/standing_still.png';
         this.image = this.standingStillImage;
+
+        const dinoRunImage1 = new Image();
+        dinoRunImage1.src = 'images/dino_run1.png';
+
+        const dinoRunImage2 = new Image();
+        dinoRunImage2.src = 'images/dino_run2.png';
     }
 
     update(gameSpeed, frameTimeDelta) {
-        
+
     }
 
     draw() {
